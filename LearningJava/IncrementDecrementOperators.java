6b234226fd941b2e55c3a58d182041e6d6cdb208
tomasgz7 <tomasguzmancabj@hotmail.com>
2025-10-11 05:48:40 -0300

    Add increment and decrement operator examples

---
+++ LearningJava/IncrementDecrementOperators.java
@@ -0,0 +1,14 @@
+package LearningJava;
+
+public class IncrementDecrementOperators {
+    
+    public static void main(String[] args) {
+        int x = 10, y, z;
+        y = ++x; 
+        z = --x;
+        
+
+        System.out.println("\n"+y);
+        System.out.println(z);
+    }
+}
