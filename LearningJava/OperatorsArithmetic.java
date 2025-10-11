ba63ebc4729d477d26b5131949c9f8a8e07471ae
tomasgz7 <tomasguzmancabj@hotmail.com>
2025-10-11 04:39:29 -0300

    Arithmetic assignment operators

---
+++ LearningJava/OperatorsArithmetic.java
@@ -0,0 +1,20 @@
+package LearningJava;
+
+public class OperatorsArithmetic {
+    public static void main(String[] args) {
+        int number = 10;
+        number += 5;
+         System.out.println("\nResult of += " + number);
+        number -= 8;
+         System.out.println("Result of -= " + number);
+        number *= 10;
+        System.out.println("Result of *= " + number);
+        number /= 3;
+         System.out.println("Result of /= " + number);
+        number %= 4;
+        System.out.println("Result of %= " + number);
+        
+       
+
+    }
+}
