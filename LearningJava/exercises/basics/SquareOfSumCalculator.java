3471a7876fafc6e1f5d74846ea96b20ed17259f6
tomasgz7 <tomasguzmancabj@hotmail.com>
2025-10-13 06:21:42 -0300

    Add SquareOfSumCalculator program to compute (a + b)²

---
+++ LearningJava/exercises/basics/SquareOfSumCalculator.java
@@ -0,0 +1,26 @@
+package LearningJava.exercises.basics;
+/*  
+ * Write a program that calculates the square of a sum.
+ */
+import java.util.Scanner;
+
+public class SquareOfSumCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        float a, b, ab;
+
+        System.out.println("Enter number: ");
+        a = input.nextFloat();
+
+        System.out.println("Enter second number: ");
+        b = input.nextFloat();
+
+        ab = (a * a) + (b * b) +  (2 * a * b );
+
+        System.out.println("The square of the sum: "+ ab);
+
+        input.close();
+
+    }
+    
+}
