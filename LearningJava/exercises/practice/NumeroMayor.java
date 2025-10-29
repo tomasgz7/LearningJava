4a922b42e188a681f04f6045c4941825028f0e20
Tomas B. Ezequiel Guzman <tomasgz.dev@gmail.com>
2025-10-29 00:26:39 -0300

    Exercise: Search num with for and if

---
+++ LearningJava/exercises/practice/NumeroMayor.java
@@ -0,0 +1,17 @@
+package LearningJava.exercises.practice;
+
+public class NumeroMayor {
+    public static void main(String[] args) {
+
+        int [] nums = {2, 5, 10, 17, 6, 4};
+        int mayor = 0;
+
+        for (int i = 0; i < nums.length; i ++) {
+            if (nums [i] > mayor){
+                mayor = nums[i];
+            }
+        }
+        System.out.println("El numero mayor es: " +mayor);
+    }
+
+}
